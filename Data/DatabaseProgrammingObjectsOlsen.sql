71fb52293bf0819631f3b4509315c202561ab698
no00015 <no00015@mix.wvu.edu>
2026-02-19 00:09:52 -0500

    Stuck on the queries, not 100% sure what direction I need to follow. I spent a while unsure where to continue

--- Data/DatabaseProgrammingObjectsOlsen.sql
+++ Data/DatabaseProgrammingObjectsOlsen.sql
@@ -1,2 +1,9 @@
 -- 3 queries
 -- 1 each for ConferenceDivision and Team Tables, and 1 join query
+
+
+select * from Team;
+
+select * from ConferenceDivision
+inner join Team on ConferenceDivision.DivisionID = Team.DivisionID;
+    
